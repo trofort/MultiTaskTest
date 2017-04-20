a64227d7a78c531eff08e903f44a44d0fccdd0d7
Trofort <Snailkick1@gmail.com>
2017-04-20 14:17:35 +0300

    Initial Commit

---
+++ MultiTaskTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MultiTaskTest
+//
+//  Created by Maksim Dehanov on 20.04.17.
+//  Copyright © 2017 Maksim Dehanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ MultiTaskTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiTaskTest
+//
+//  Created by Maksim Dehanov on 20.04.17.
+//  Copyright © 2017 Maksim Dehanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
